6e86bcc6c4ca82dbb02a87480697b43ad19ab311
Guilherme Sousa <guilherme3s@hotmail.com>
2023-03-20 20:06:58 -0300

    Media funcionando

--- src/components/Form/Result/index.js
+++ src/components/Form/Result/index.js
@@ -2,12 +2,12 @@ import { Text, View } from "react-native";
 import styles from "./style";
 
 
-export default function Result(){
+export default function Result(props){
     return(
         <View style={styles.resultadoMedia}>
             <Text style={styles.info}>Sua Média foi:</Text>
-            <Text style={styles.media}>8.0</Text>
-            <Text style={[styles.msgResultado, styles.corAprovado]}>Mensagem</Text>
+            <Text style={styles.media}>{props.media}</Text>
+            <Text style={[styles.msgResultado, styles.corAprovado]}>{props.msgResultado}</Text>
         </View>
     )
 }
